79573d550659818ef553fc0a83e2d6691ade44de
Jason Zhu <55606978+jasonmeme@users.noreply.github.com>
2025-10-09 13:54:02 -0400

    Update unit_testing.tsx

--- client/src/routes/unit_testing.tsx
+++ client/src/routes/unit_testing.tsx
@@ -3,12 +3,6 @@ import { createFileRoute } from "@tanstack/react-router";
 import {
     testProfileDataIntegrity,
     testMessagingAndAPI,
-    testEndCallFlow,
-    testFeedback,
-    testMatchSearch,
-    testProfileOnCall,
-    testMatchingOptions,
-    testViewingProfiles,
     UnitTestModule,
     type UnitTestType
 } from "@/components/UnitTest";
@@ -31,42 +25,6 @@ function RouteComponent() {
             name: "Messaging & API Endpoints",
             count: 3,
             func: testMessagingAndAPI,
-        },
-        {
-            id: 3,
-            name: "End Call UI & Home Page Summaries",
-            count: 4,
-            func: testEndCallFlow,
-        },
-        {
-            id: 4,
-            name: "Feedback Functionality",
-            count: 3,
-            func: testFeedback,
-        },
-        {
-            id: 5,
-            name: "Match Search & Filtering",
-            count: 3,
-            func: testMatchSearch,
-        },
-        {
-            id: 6,
-            name: "Profile Info During Call",
-            count: 3,
-            func: testProfileOnCall,
-        },
-        {
-            id: 7,
-            name: "Matching Options",
-            count: 3,
-            func: testMatchingOptions,
-        },
-        {
-            id: 8,
-            name: "Viewing Other Profiles",
-            count: 5,
-            func: testViewingProfiles,
         },
     ];
 
