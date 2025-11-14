a63071f23d3f83b86523919503346448fd3d3230
Zach McMullen <87542111+ZachMcM@users.noreply.github.com>
2025-11-14 11:01:36 -0500

    fix

--- server/src/utils/auth.ts
+++ server/src/utils/auth.ts
@@ -7,7 +7,6 @@ import { sendEmail } from "./mailer";
 import { eq } from "drizzle-orm";
 
 export const auth = betterAuth({
-  baseURL: process.env.BETTER_AUTH_URL!,
   database: drizzleAdapter(db, {
     provider: "pg",
     schema,
